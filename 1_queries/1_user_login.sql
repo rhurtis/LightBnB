2dc9d37e466997d186a967ccc57c38a650391021
Ryan H <rhurtis@hotmail.com>
2020-08-20 05:14:41 +0000

    Added 5 queries.

---
+++ 1_queries/1_user_login.sql
@@ -0,0 +1,4 @@
+/* Get details about a single user. This is for when the user logs in. */
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
---
+++ 1_queries/2_average_length_of_reservation.sql
@@ -0,0 +1,3 @@
+/* Get the average duration of all reservations. */
+SELECT AVG(end_date - start_date) AS average_duration
+FROM reservations;
