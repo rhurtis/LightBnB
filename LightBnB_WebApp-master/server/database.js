2788051f68f8db96f9a706ae66b2ba6edfd1bd1c
Ryan H <rhurtis@hotmail.com>
2020-08-24 00:16:00 +0000

    Changed the getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -124,13 +124,63 @@ exports.getAllReservations = getAllReservations;
 // }
 
 // using the getAllProperties function to query the lighbnb database.
+// const getAllProperties = function(options, limit = 10) {
+//   return pool.query(`
+//   SELECT * FROM properties
+//   LIMIT $1
+//   `, [limit])
+//   .then(res => res.rows);
+// }
+
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += `WHERE owner_id LIKE $${queryParams.length} `;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(`%${options.minimum_price_per_night * 100}%`);
+    queryString += `WHERE minimum_price_per_night LIKE $${queryParams.length} `;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(`%${options.maximum_price_per_night * 100}%`);
+    queryString += `WHERE maximum_price_per_night LIKE $${queryParams.length} `;
+  }
+  if (options.minimum_rating) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE minimum_rating LIKE $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
+
 exports.getAllProperties = getAllProperties;
 
 
